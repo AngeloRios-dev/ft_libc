66ad045a850ecb6b25a4006edd357b999368e4e0
Angelo Rios <angelorios@live.com>
2025-04-23 19:51:26 +0200

    add: fn ft_memcpy

--- Makefile
+++ Makefile
@@ -8,7 +8,8 @@ SRC = ft_isalpha.c \
 	ft_isprint.c \
 	ft_strlen.c \
 	ft_memset.c \
-	ft_bzero.c
+	ft_bzero.c \
+	ft_memcpy.c
 OBJ = $(SRC:.c=.o)
 NAME = libft.a
 
@@ -24,4 +25,3 @@ fclean: clean
 	 rm -f $(NAME)
 
 re: fclean all
-
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,27 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/09 22:10:42 by mochi             #+#    #+#             */
-/*   Updated: 2025/04/23 17:19:47 by angrios          ###   ########.fr       */
+/*   Created: 2025/04/23 16:35:43 by angrios           #+#    #+#             */
+/*   Updated: 2025/04/23 18:21:00 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdio.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_strlen(const char *s);
-void	*ft_memset(void *str, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr_dest;
+	unsigned char	*ptr_src;
 
-#endif
+	i = 0;
+	ptr_dest = (unsigned char *)dest;
+	ptr_src = (unsigned char *)src;
+	if (dest == src)
+		return (dest);
+	while (i < n)
+	{
+		ptr_dest[i] = ptr_src[i];
+		i++;
+	}
+	return (dest);
+}
