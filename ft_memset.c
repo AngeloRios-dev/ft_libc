6bd77acf9f9fb3e0aeb24696e731906fa304c265
Angelo Rios <angelorios@live.com>
2025-04-23 09:26:13 +0200

    ref: chg i type & fn prototype

--- ft_memset.c
+++ ft_memset.c
@@ -3,26 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angrios <angrios@student.42.fr>            +#+  +:+       +#+        */
+/*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 11:05:40 by angrios           #+#    #+#             */
-/*   Updated: 2025/04/21 13:34:09 by angrios          ###   ########.fr       */
+/*   Updated: 2025/04/23 09:23:45 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *str, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
-	unsigned int	i;
-	unsigned char	*p_str;
+	size_t			i;
+	unsigned char	*ptr_s;
 
 	i = 0;
-	p_str = (unsigned char *)str;
+	ptr_s = (unsigned char *)s;
 	while (i < n)
 	{
-		p_str[i] = (unsigned char)c;
+		ptr_s[i] = (unsigned char)c;
 		i++;
 	}
-	return (str);
+	return (s);
 }
