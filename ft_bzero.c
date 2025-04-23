344b2c6cbcfde8c8998bed1fe716935565307dda
Angelo Rios <angelorios@live.com>
2025-04-23 09:05:39 +0200

    ref: change i type & 0 instead of \0

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 16:52:06 by angrios           #+#    #+#             */
-/*   Updated: 2025/04/22 18:59:34 by angrios          ###   ########.fr       */
+/*   Updated: 2025/04/23 09:00:36 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned int	i;
+	size_t	i;
 	unsigned char	*ptr_s;
 
 	i = 0;
 	ptr_s = (unsigned char *)s;
-	while (i < n && n > 0)
+	while (i < n)
 	{
-		ptr_s[i] = '\n';
+		ptr_s[i] = 0;
 		i++;
 	}
 }
