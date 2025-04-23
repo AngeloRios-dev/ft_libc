f3b2a65cec868868ab9e42cb15abedd2bf51595a
Angelo Rios <angelorios@live.com>
2025-04-23 21:05:58 +0200

    wip: fn ft_memove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,28 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/09 22:10:42 by mochi             #+#    #+#             */
-/*   Updated: 2025/04/23 20:29:56 by angrios          ###   ########.fr       */
+/*   Created: 2025/04/23 19:56:48 by angrios           #+#    #+#             */
+/*   Updated: 2025/04/23 20:51:25 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdio.h>
+#include "libft.h"
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_strlen(const char *s);
-void	*ft_memset(void *str, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	size_t			j;
+	unsigned char	*ptr_dest;
+	unsigned char	*ptr_src;
 
-#endif
+	i = 0;
+	j = 0;
+	ptr_dest = (unsigned char *)dest;
+	ptr_src = (unsigned char *)src;
+	if (dest == src)
+		return (dest);
+
+	return (dest);
+}
