8fb41702d6d89796423074716c5c0f796e20181f
Angelo Rios <angelorios@live.com>
2025-04-24 21:39:29 +0200

    ref: finish working on fn ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 19:56:48 by angrios           #+#    #+#             */
-/*   Updated: 2025/04/23 20:51:25 by angrios          ###   ########.fr       */
+/*   Updated: 2025/04/24 19:26:38 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,27 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t			i;
-	size_t			j;
 	unsigned char	*ptr_dest;
 	unsigned char	*ptr_src;
 
-	i = 0;
-	j = 0;
 	ptr_dest = (unsigned char *)dest;
 	ptr_src = (unsigned char *)src;
-	if (dest == src)
+	if (ptr_dest == ptr_src)
 		return (dest);
-
+	if (ptr_dest > ptr_src)
+	{
+		i = n;
+		while (i-- > 0)
+			ptr_dest[i] = ptr_src[i];
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			ptr_dest[i] = ptr_src[i];
+			i++;
+		}
+	}
 	return (dest);
 }
