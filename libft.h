79607b0ebc41fc04b4fc3c4bbeac29df2334b3e4
Angelo Rios <angelorios@live.com>
2025-04-25 14:42:58 +0200

    add: fn ft_strlcpy.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 22:10:42 by mochi             #+#    #+#             */
-/*   Updated: 2025/04/23 20:29:56 by angrios          ###   ########.fr       */
+/*   Updated: 2025/04/25 12:53:28 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
