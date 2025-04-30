a98866d07db71c2e2e9f532a82dd12243bd9e772
Angelo Rios <angelorios@live.com>
2025-04-30 14:30:10 +0200

    wip: working on ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 14:44:41 by angrios           #+#    #+#             */
-/*   Updated: 2025/04/25 16:51:12 by angrios          ###   ########.fr       */
+/*   Updated: 2025/04/30 13:28:58 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dst_len;
 	size_t	src_len;
-	
+	int		rtn;
+
 	dst_len = 0;
 	src_len = 0;
+	rtn = ft_strlen(dst) + ft_strlen(src);
 	while (dst[dst_len] != '\0')
 		dst_len++;
-	while (src[src_len] != '\0' && src[src_len] < size)
+	while (src[src_len] != '\0' && dst_len < size)
 	{
 		dst[dst_len] = src[src_len];
 		src_len++;
 		dst_len++;
 	}
 	dst[dst_len] = '\0';
-	
+	return (rtn);
 }
 
 #include <stdio.h>
