5e08f1d7023855ab67bc804ad6a0d2119eb1f53a
Angelo Rios <angelorios@live.com>
2025-05-01 16:30:28 +0200

    add: function ft_toupper

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 22:10:42 by mochi             #+#    #+#             */
-/*   Updated: 2025/04/30 13:08:58 by angrios          ###   ########.fr       */
+/*   Updated: 2025/05/01 16:28:31 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_toupper(int c);
 
 #endif
