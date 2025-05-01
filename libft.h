aa97566a970e5c10554783fa3fe1ceb9791b884d
Angelo Rios <angelorios@live.com>
2025-05-01 21:51:35 +0200

    feat: implements function ft_memchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 22:10:42 by mochi             #+#    #+#             */
-/*   Updated: 2025/05/01 19:02:39 by angrios          ###   ########.fr       */
+/*   Updated: 2025/05/01 21:26:45 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
