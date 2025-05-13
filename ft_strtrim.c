e347fe8ca6a9746c5ef2f37bd8f649cc6f958de1
Angelo Rios <angelorios@live.com>
2025-05-13 16:44:29 +0200

    ref: make use of ft_strchr to simplify

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,42 +6,11 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 17:21:29 by angrios           #+#    #+#             */
-/*   Updated: 2025/05/11 19:13:15 by angrios          ###   ########.fr       */
+/*   Updated: 2025/05/13 16:41:28 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	ft_char_in_set(char c, const char *set)
-{
-	while (*set)
-	{
-		if (c == *set)
-			return (1);
-		set++;
-	}
-	return (0);
-}
-
-static int	ft_starting_char(const char *s1, const char *set)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i] && ft_char_in_set(s1[i], set))
-		i++;
-	return (i);
-}
-
-static int	ft_ending_char(const char *s1, const char *set, int len)
-{
-	int	i;
-
-	i = len - 1;
-	while (i >= 0 && ft_char_in_set(s1[i], set))
-		i--;
-	return (i);
-}
 
 char	*ft_strtrim(const char *s1, const char *set)
 {
@@ -51,11 +20,15 @@ char	*ft_strtrim(const char *s1, const char *set)
 
 	if (!s1 || !set)
 		return (NULL);
-	start = ft_starting_char(s1, set);
-	end = ft_ending_char(s1, set, ft_strlen(s1));
-	trimmed = malloc((end - start + 2) * sizeof(char));
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	trimmed = malloc((end - start + 1) * sizeof(char));
 	if (!trimmed)
 		return (NULL);
-	ft_strlcpy(trimmed, s1 + start, end - start + 2);
+	ft_strlcpy(trimmed, &s1[start], end - start + 1);
 	return (trimmed);
 }
