3e1cd5a94eaad795082e73e779e705f1f6f8872b
Angelo Rios <angelorios@live.com>
2025-05-13 17:39:35 +0200

    wip: implementation of ft_split

---
+++ ft_split.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 16:56:02 by angrios           #+#    #+#             */
+/*   Updated: 2025/05/13 17:33:21 by angrios          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_word_counter(const char *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		else
+			i++;
+	}
+	return (count);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	
+}
