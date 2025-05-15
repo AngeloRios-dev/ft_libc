ede50c08ed5222a2defafbfcd2aca60f311d0291
Angelo Rios <angelorios@live.com>
2025-05-15 15:25:34 +0200

    doc: ft_atoi, ft_bzero, ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,17 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 13:17:15 by angrios           #+#    #+#             */
-/*   Updated: 2025/05/14 18:55:44 by angrios          ###   ########.fr       */
+/*   Updated: 2025/05/15 15:24:20 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+The function allocates memory for an array of NMEMB elements 
+of SIZE bytes each and returns a pointer to the allocated memory.
+The memory is set to zero.
+*/
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t	mem_size;
